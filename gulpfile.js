d22e76c9451973cf5304121cde3e64d989b4da54
Michal Kononenko <michalkononenko@gmail.com>
2015-11-15 22:44:44 -0500

    Added a very simple working gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-var sourcemaps = requre('gulp-sourcemaps');
-var concat = requrire("gulp-concat")
+'use strict';
 
-gulp.task('default', function() {
-    return gulp.src("src/app.js")
-        .pipe(babel())
-        .pipe(concat("all.js"))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist"));
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+
+gulp.task('build', function () {
+    browserify({
+        entries: 'index.js',
+        extensions: ['.js', '.jsx'],
+        debug: true
+    })
+        .transform(babelify.configure({
+            presets: ["es2015", "react"]
+        }))
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['build']);
---
+++ hello-world.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Michal on 2015-11-15.
+ */
+
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render() {
+        return <p>Hello, world!</p>;
+    }
+}
+
+export default HelloWorld;
