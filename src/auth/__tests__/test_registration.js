2991d3f551851cc9c73e2b61a3442cb814cec489
Michal Kononenko <michalkononenko@gmail.com>
2015-12-07 17:24:22 -0500

    Started writing code for login form

---
+++ src/auth/__tests__/test_registration.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Michal on 2015-12-07.
+ */
+'use strict';
+
+import React from 'react';
+import TestUtils from 'react/lib/ReactTestUtils';
+import expect from 'expect';
+import LoginForm from '../registration';
+
+describe('Test the LoginForm for the front page', () => {
+    it('Should pass properties from the superclass constructor in its constructor',
+        () => {
+            var constructor_call_args = [];
+
+            React.Component.constructor = (props) => {
+                constructor_call_args.push(props)
+            };
+
+            var props = {name: 'foo'};
+
+            new LoginForm(props);
+        });
+
+    it('')
+});
