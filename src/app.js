50bbb9ef4961e5ffadc8a81c47cc90f55eeccc80
Michal Kononenko <michalkononenko@gmail.com>
2015-12-22 14:02:24 -0500

    Added flux as a dependency

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * Bootstraps the application
+ *
+ * Created by Michal on 2015-12-19.
+ */
+
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app-container')
+);
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Michal on 2015-12-19.
+ */
+import React from 'react';
+
+class App extends React.Component {
+
+    static render() {
+        return(
+            <h1>This is the app</h1>
+        );
+    };
+}
+
+export default App;
