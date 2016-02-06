f32c1f426ccddf691e4e0b8cf875662eacced4d0
Michal Kononenko <michalkononenko@gmail.com>
2016-02-06 14:41:19 -0500

    Added documentation and file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
                 loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.jpe?g$/,
+                loader: "file"
             }
         ]
     },
