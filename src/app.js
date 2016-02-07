4659a957cc1305640a7f56e5dba3a0ff61556d0a
Michal Kononenko <michalkononenko@gmail.com>
2016-02-07 14:37:05 -0500

    Removed distinct class names for top-level App component

--- src/app.js
+++ src/app.js
@@ -20,31 +20,21 @@ class App extends React.Component {
      * Constructs the base method, takes in a unique name for the
      * app component, and sets the props and state for the component
      *
-     * @param {string} className The name of the instance of this component
      * @param {object} props The properties of this component
      * @param {object} state The set of variables making up this component's state
      */
-    constructor(className, props, state){
+    constructor(props, state){
         super(props, state);
-        if (className === undefined) {
-            throw Error('A Generic App object was instantiated without a className')
-        }
-        if (props === undefined) {
-            console.log(
-                'Props undefined for class ' + String(className) + '. Using {} instead'
-            );
+        if (props === undefined){
             this.props = {};
         } else {
             this.props = props;
         }
 
         if (state === undefined) {
-            console.log(
-                'State undefined for class ' + String(className) + '. Using {} instead'
-            );
-            this.state = {}
+            this.state = {};
         } else {
-            this.state = state;
+            this.state = state
         }
     }
 
