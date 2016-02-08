94474eba41975f1231f4b4ff8b28431716f2cfbf
Michal Kononenko <michalkononenko@gmail.com>
2016-02-08 11:23:40 -0500

    Changed name of HTTP_test

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import React from 'react';
 import Logo from './components/logo';
 import LoginForm from './components/LoginForm';
 import HeaderBar from './components/header';
-import HTTPTest from './components/cross_origin_http';
+import HTTPTest from './components/http_cors_test';
 import {Router, Route, Link, browserHistory} from 'react-router';
 
 import "bootstrap/dist/css/bootstrap.min.css";
