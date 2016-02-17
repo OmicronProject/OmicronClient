90634d5177113b88260564a459c1526213856783
Michal Kononenko <michalkononenko@gmail.com>
2016-02-17 00:19:56 -0500

    More docs

--- src/containers/header.js
+++ src/containers/header.js
@@ -12,6 +12,7 @@ import { connect } from 'react-redux';
  * state
  *
  * @param buttons The buttons to place in the header bar
+ * @constructor
  */
 export const Header = ({buttons}) => (
     <div id="header_bar" className="container container-fluid">
@@ -21,6 +22,16 @@ export const Header = ({buttons}) => (
     </div>
 );
 
+/**
+ * Factory for creating a button in the menu. Differentiates between buttons
+ * pointing to internal links in the single-page application and external
+ * links to other websites. External links could include a potential Wordpress
+ * blog for a spin physics wiki authenticated through OAuth 2 (nudge nudge
+ * wink wink :))
+ *
+ * @param {Object} button The button to be made
+ * @private
+ */
 export function _make_button(button) {
     if (button.type === "internal"){
         return _make_internal(button);
@@ -29,6 +40,14 @@ export function _make_button(button) {
     }
 }
 
+/**
+ * Make an internal link to another page in the Omicron Client's SPA, using the
+ * Link element provided by react-router.
+ *
+ * @param {Object} button The button to make
+ * @returns {XML} The JSX representation of the link
+ * @private
+ */
 export function _make_internal(button){
     return(
         <li key={button.key} role="presentation">
