64ed019944f4dcd2ced15c4767f7ad70dd218c97
Michal Kononenko <michalkononenko@gmail.com>
2016-02-17 11:21:53 -0500

    Wrote more unit tests for the http_request.

--- src/containers/__tests__/test_http_request.js
+++ src/containers/__tests__/test_http_request.js
@@ -7,6 +7,7 @@ import expect from 'expect';
 import React from 'react';
 import {HTTPTestTemplate, map_state_to_props} from '../http_request';
 import {map_dispatch_to_props} from '../http_request';
+import {URL_CHANGED, url_changed, url_changed_reducer} from '../http_request';
 import Header from '../header';
 
 describe("HTTPTestTemplate", () => {
@@ -63,4 +64,46 @@ describe("map_dispatch_to_props", () => {
             expect(typeof result.on_url_change).toEqual("function");
             expect(typeof result.on_button_click).toEqual("function");
         })
+});
+
+describe("url_changed action creator", () => {
+    let new_url;
+
+    beforeEach(() => {
+        new_url = 'foo'
+    });
+
+    it('should create an action of the URL_CHANGED type', () => {
+        let action = url_changed(new_url);
+
+        expect(action.type).toEqual(URL_CHANGED);
+        expect(action.new_url).toEqual(new_url);
+    })
+});
+
+describe("url_changed reducer", () => {
+    let state;
+    let action;
+    let new_url;
+
+    beforeEach(() => {
+        new_url = 'https://api.github.com';
+        action = url_changed(new_url);
+
+        state = {http_test: {url: "https://api.google.com"}}
+    });
+
+    it("Should return the old state if the action is not URL_CHANGED", () => {
+        let not_proper_action = {type: "GENERIC_ACTION", new_url: new_url};
+
+        expect(not_proper_action.type).toNotEqual(URL_CHANGED);
+
+        expect(url_changed_reducer(state, not_proper_action)).toEqual(state);
+    });
+
+    it("Should change the URL if the action is URL_CHANGED", () => {
+        let new_state = {http_test: {url: new_url}};
+
+        expect(url_changed_reducer(state, action)).toEqual(new_state);
+    })
 });
