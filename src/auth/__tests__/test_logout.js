13010a163c2dda3f680da088d1dad23f129cfb5b
Michal Kononenko <michalkononenko@gmail.com>
2016-03-15 10:46:27 -0400

    A few more tests.

--- src/auth/__tests__/test_logout.js
+++ src/auth/__tests__/test_logout.js
@@ -6,6 +6,8 @@ import React from 'react';
 import expect from 'expect';
 
 import {LOGOUT_STARTED, logout_started, logout_started_reducer} from '../logout';
+import {SEND_LOGOUT_REQUEST, send_logout_request} from '../logout';
+import {send_logout_request_reducer} from '../logout';
 
 describe(LOGOUT_STARTED, () => {
     describe('action creator', () => {
@@ -34,5 +36,29 @@ describe(LOGOUT_STARTED, () => {
                 {is_logging_out: true, has_logged_out: false}
             )
         });
+    });
+});
+
+describe(SEND_LOGOUT_REQUEST, () => {
+    describe("action creator", () => {
+        it("Should create the action", () => {
+            expect(send_logout_request()).toEqual(
+                {type: SEND_LOGOUT_REQUEST}
+            )
+        })
+    });
+
+    describe("reducer", () => {
+        let state;
+        beforeEach(() => {
+            state = {auth: {back_end: {is_logging_out: false}}}
+        });
+
+        it("should reduce", () => {
+            send_logout_request_reducer(state);
+            expect(state.auth.back_end.is_logging_out).toEqual(
+                true
+            )
+        })
     })
 });
