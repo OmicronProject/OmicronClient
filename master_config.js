ad7594ac90bcc4cffd22e444511509583ae772f1
Michal Kononenko <michalkononenko@gmail.com>
2016-04-15 14:04:58 -0400

    Repointed API to omicron server.

--- master_config.js
+++ master_config.js
@@ -5,7 +5,7 @@
  *
  */
 'use strict';
-let api_url = 'http://localhost:5000';
+let api_url = 'https://omicronserver.herokuapp.com';
 let github_repo_url = 'https://github.com/MichalKononenko/OmicronClient';
 let waffle_url = "https://waffle.io/MichalKononenko/OmicronClient";
 let esdoc_url = "https://doc.esdoc.org/github.com/MichalKononenko/OmicronClient/";
